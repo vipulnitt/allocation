5ca3e5902970856a9e5d914651b89930bdb5b39a
Vipul Patel <32597042+vipulnitt@users.noreply.github.com>
2023-06-25 13:24:26 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
 const connectDatabase = () =>{
-    mongoose.connect(process.env.DB_LOCAL_URI,{}).then(con=>{
+    mongoose.connect('mongodb+srv://vp98504:vipul98504@allocation.zetm3om.mongodb.net/?retryWrites=true&w=majority',{}).then(con=>{
         console.log(`mongoDB database connected with host: ${con.connection.host}`)
 })
 
