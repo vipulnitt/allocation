2c77626be149b7682940f744dad69bda0e104d65
vipulnitt <vp98504@gmail.com>
2023-06-26 08:13:09 +0530

    abc

--- frontend/src/routes/ProtectedRoute.js
+++ frontend/src/routes/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ isLoggedIn,children }) => {
-  //const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token');
 
   if (!isLoggedIn) {
     return <Navigate to="/" replace />;
