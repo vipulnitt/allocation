b72bd796b496339867a8b6a3b62f7583a3c8e02f
vipulnitt <vp98504@gmail.com>
2023-06-26 12:36:57 +0530

    abcd

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,32 @@
 const express = require('express');
-const app=express();
+const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const errorMiddleware = require('./middleware/errors');
-var bodyParser = require('body-parser')
-var cors = require('cors')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const corsOptions ={
-    origin:'https://allocation-frontend.vercel.app/', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: 'https://allocation-frontend.vercel.app',
+  credentials: true,
+  optionSuccessStatus: 200
+}));
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const admin = require('./routes/admin');
 const user = require('./routes/user');
 const form = require('./routes/form');
 const formDetails = require('./routes/formDetails');
 
-app.use('/api/v1',admin);
-app.use('/api/v1',user);
-app.use('/api/v1',form);
-app.use('/api/v1',formDetails);
+app.use('/api/v1', admin);
+app.use('/api/v1', user);
+app.use('/api/v1', form);
+app.use('/api/v1', formDetails);
 
-
-//middleware to handle errors
+// middleware to handle errors
 app.use(errorMiddleware);
+
 module.exports = app;
