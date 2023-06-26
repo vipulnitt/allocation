a82dfde1359eff6dea78d90b310df4af59110ca2
vipulnitt <vp98504@gmail.com>
2023-06-26 12:21:41 +0530

    abcd

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser')
 var cors = require('cors')
 
 const corsOptions ={
-    origin:'https://allocation-frontend.vercel.app', 
+    origin:process.env.ORIGIN_URL, 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
